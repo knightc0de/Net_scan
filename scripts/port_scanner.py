43609332e278d7344e68c3ba097f0481fca5f434
knightc0de <its_puneet143@proton.me>
2025-11-03 09:47:16 +0530

    building udp scan

--- scripts/port_scanner.py
+++ scripts/port_scanner.py
@@ -6,12 +6,9 @@ from termcolor import colored
 from  threading import Thread,Lock
 
 class Net_Scan():
-<<<<<<< HEAD
+
       def __init__(self,ip,start_ports,end_ports,threads=500,verbose=False,proto='tcp'):
           self.proto = proto
-=======
-      def __init__(self,ip,start_ports,end_ports,threads=500,verbose=False):
->>>>>>> 77cd48efe1513149522e341acdf590a7f45209e7
           self.ip = ip
           self.start_port = start_ports
           self.end_port = end_ports
@@ -21,9 +18,6 @@ class Net_Scan():
           self.lock = Lock()
           print(colored((f"[+] Scaning {self.ip}"),"green"))
 
-
-      def mac_dectect():
-          pass   
 
             
       def threads_handling(self):
@@ -41,38 +35,43 @@ class Net_Scan():
            print(colored(f"Scan completed in {pro_end_time - pro_start_time:.2f} seconds","green"))
       
       def scan_ports(self):
-          while True:
-            try:
-                port = next(self.ports)
-            except StopIteration:
-                   break  
-            try:
-               s = socket.socket()
-               s.settimeout(1)
-               s.connect((self.ip, port))
-               with self.lock:
-                print(f"[+] Port {port} is open")
-            except PermissionError as e:
-                    with self.lock:
-                     sys.stderr.write(colored(f"[!] Permission denied on port {port}: {e}\n", "red"))
-            except (ConnectionRefusedError, socket.timeout):
-                 if self.verbose:
-                  with self.lock:
-                   sys.stderr.write(colored(f"[-] Port {port} is closed or timed out\n","red"))
-                   sys.exit(1)        
+       # tcp 
+         if self.proto in ('tcp' , 'both') :
+           while True:
+             try:
+                 port = next(self.ports)
+             except StopIteration:
+                    break  
+             try:
+                s = socket.socket()
+                s.settimeout(1)
+                s.connect((self.ip, port))
+                with self.lock:
+                 print(f"[+] Port {port} is open")
+             except PermissionError as e:
+                     with self.lock:
+                      sys.stderr.write(colored(f"[!] Permission denied on port {port}: {e}\n", "red"))
+             except (ConnectionRefusedError, socket.timeout):
+                  if self.verbose:
+                   with self.lock:
+                    sys.stderr.write(colored(f"[-] Port {port} is closed or timed out\n","red"))
+                    sys.exit(1)        
+            # udp 
+            
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Just A Port Scanner",
                             epilog='Example: python %(prog)s -H 192.168.1.10 -s 1 -e 65535 -t 500 -v')
 
     parser.add_argument("--host", help="Target IPv4", dest="host", required=True)
+    parser.add_argument("--proto",help="Protocol to scan: tcp, udp, or both",dest='proto',choices=['tcp', 'udp', 'both'], default='tcp')
     parser.add_argument("-s", help="Starting Port", dest='start_ports', type=int, default=1)
     parser.add_argument("-e", help="Ending Port", dest='end_ports', type=int, default=65535)
     parser.add_argument("-t", help="Number of threads", dest='threads', type=int, default=500)
     parser.add_argument("-v", "--verbose", help="Verbose mode", dest='verbose', action='store_true')
-
+    
     args = parser.parse_args()
-    scanner = Net_Scan(args.host, args.start_ports, args.end_ports, args.threads, args.verbose)
+    scanner = Net_Scan(args.host, args.start_ports, args.end_ports, args.threads, args.verbose,proto=args.proto)
     scanner.threads_handling()
 
 
