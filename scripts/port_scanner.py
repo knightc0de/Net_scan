7aa4887d6f30f9070f3618cd7e39d0cb2dc6e793
Puneet Singh <its_puneet143@proton.me>
2025-11-07 09:39:06 +0530

    Add mac_detection function placeholder
    Added a placeholder function for MAC detection.

--- scripts/port_scanner.py
+++ scripts/port_scanner.py
@@ -19,7 +19,10 @@ class Net_Scan():
           print(colored((f"[+] Scaning {self.ip}"),"green"))
 
 
-            
+      def mac_detection():
+            pass
+
+      
       def threads_handling(self):
            pro_start_time = time.time()
            thread_list = []
