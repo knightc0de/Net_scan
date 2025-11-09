34ed83f9fca3b6483aed12f8d51c53a7087b9afa
knightc0de <its_puneet143@proton.me>
2025-11-09 12:26:22 +0530

    building network env packets

--- scripts/mac_detect.py
+++ scripts/mac_detect.py
@@ -1,6 +1,14 @@
 import sys
-import scapy
+import scapy.all
 
 
 class Mac_detect():
+      def __init__(self,host):
+            self.host = host 
       
+      def packet(self):
+            layer_1 = Ether(dst="ff:ff:ff:ff:ff:ff")
+            layer_2 = ARP()
+
+            
+            
