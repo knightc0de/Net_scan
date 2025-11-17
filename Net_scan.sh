e8206ce6ab994e45c40b38a889e0a00649210048
Puneet Singh <its_puneet143@proton.me>
2025-11-17 17:53:21 +0530

    Fix prompt for IP range input in MAC scanner

--- Net_scan.sh
+++ Net_scan.sh
@@ -57,7 +57,7 @@ case "$opt" in
   
   3) 
       echo -e "${YELLOW}\n[~] Launching MAC Detection Scanner...${NC}"
-      read ip "network rage " host
+      read -p "Target IP Range (e.g., 192.168.1.0/24): " net_range
   4)
     echo -e "${RED} Exiting... ${NC}"
     exit 0
